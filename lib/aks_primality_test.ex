33328af7acbae2efb1a7e5d460e1ec10b176ba4d
Susumu Yamazaki <zacky1972@gmail.com>
2025-07-21 06:27:23 +0900

    initial commit

---
+++ lib/aks_primality_test.ex
@@ -0,0 +1,18 @@
+defmodule AKSPrimalityTest do
+  @moduledoc """
+  Documentation for `AKSPrimalityTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AKSPrimalityTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aks_primality_test_test.exs
@@ -0,0 +1,8 @@
+defmodule AKSPrimalityTestTest do
+  use ExUnit.Case
+  doctest AKSPrimalityTest
+
+  test "greets the world" do
+    assert AKSPrimalityTest.hello() == :world
+  end
+end
